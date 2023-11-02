321fc57c73c7e745bb1464933ae327303069ff8e
SamStar <54240024+Samstar777@users.noreply.github.com>
2023-11-02 17:44:27 +0530

    Update FocusModeSettings.sh

--- FocusModeSettings.sh
+++ FocusModeSettings.sh
@@ -8,11 +8,11 @@ readSettings() {
 	# Read the focus modes setting from com.apple.controlcenter
 	focusModes=$(sudo -u "$loggedInUser" defaults read com.apple.controlcenter "NSStatusItem Visible FocusModes")
 	
-	# Check if DoNotDisturb is disabled (focusModes is 0)
+	# Check if DoNotDisturb is disabled
 	if [[ $focusModes -eq 0 ]]; then
 		echo "DoNotDisturb is Disabled"
 	else
-		# DoNotDisturb is enabled (focusModes is not 0)
+		# DoNotDisturb is enabled
 		echo "DoNotDisturb is Enabled"
 	fi
 }
